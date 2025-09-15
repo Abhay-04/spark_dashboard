6915b2f41029b2fda0760c88f4bfb5081da52636
Abhay Sharma <abhysharma5113@gmail.com>
2025-09-15 13:57:07 +0530

    Add Shadcn and Zustand
    Introduced a reusable Button component with variant and size support. Added utility functions, updated Tailwind CSS configuration, and set up path aliases in Vite and jsconfig for improved imports. Installed supporting dependencies for UI and state management, and updated App.jsx to use the new Button.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="bg-amber-600 text-3xl text-black">Hello world</h1>
+      <Button>Click me</Button>
 
     </>
   );
